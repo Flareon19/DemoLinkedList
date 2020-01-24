9448a5c38f61a1a976c6cd98900414e7ad0580f6
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-01-24 16:13:06 +0530

    make the members private

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -8,20 +8,20 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         private E data;
         private Node<E> next;
 
-        public Node(E data) {
+        private Node(E data) {
             this.data = data;
         }
 
-        public Node(E data, Node<E> next) {
+        private Node(E data, Node<E> next) {
             this.data = data;
             this.next = next;
         }
 
-        public E getData() {
+        private E getData() {
             return data;
         }
 
-        public Node<E> getNext() {
+        private Node<E> getNext() {
             return next;
         }
     }
