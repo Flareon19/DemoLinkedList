6c7691952d54fee3b91a910e8d554082fbb41002
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-01-24 16:18:26 +0530

    reformat

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -4,28 +4,6 @@ import adt.LinkedListADT;
 
 public class MyLinkedList<E> implements LinkedListADT<E> {
 
-    private static class Node<E> {
-        private E data;
-        private Node<E> next;
-
-        private Node(E data) {
-            this.data = data;
-        }
-
-        private Node(E data, Node<E> next) {
-            this.data = data;
-            this.next = next;
-        }
-
-        private E getData() {
-            return data;
-        }
-
-        private Node<E> getNext() {
-            return next;
-        }
-    }
-
     @Override
     public void add(E item) {
 
@@ -49,5 +27,29 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
     @Override
     public void print() {
 
+    }
+
+    // create the Node class
+    private static class Node<E> {
+
+        private E data;
+        private Node<E> next;
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        private E getData() {
+            return data;
+        }
+
+        private Node<E> getNext() {
+            return next;
+        }
     }
 }
